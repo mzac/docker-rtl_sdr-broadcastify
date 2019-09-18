0e044e602f891fe1d5fd6eced420cf893f896342
mzac <zachary.mcgibbon@gmail.com>
2019-09-18 10:01:57 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ RTL_DELAY="2"
 
 EZSTREAM_CONFIG="/etc/ezstream_bcfy.xml"
 
-/usr/local/bin/rtl_fm -d $RTL_DEVICE -M $RTL_MODE -f $RTL_FREQ -p $RTL_PPM -l $RTL_LEVEL -g $RTL_GAIN -t $RTL_DELAY -s 12k | \
+/usr/local/bin/rtl_fm -d $RTL_DEVICE -M $RTL_MODE -f $RTL_FREQ -p $RTL_PPM -l $RTL_LEVEL -g $RTL_GAIN -t $RTL_DELAY -E pad -s 12k | \
 /usr/bin/lame -r -s 12 --resample 22.05 -m m -b 16 --cbr --lowpass 4 - - | \
 /usr/bin/ezstream -c $EZSTREAM_CONFIG
 
